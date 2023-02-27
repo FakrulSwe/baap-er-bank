3717d69aa58907ed33a0e5bc1751bbce46e618c3
Fakrul Islam <dev.rupok02@gmail.com>
2023-02-27 22:57:00 +0600

    Varify Email & Pass

--- js/login.js
+++ js/login.js
@@ -1,3 +1,21 @@
+// Step 1: Add event handler with the submit button
 document.getElementById("btn-submit").addEventListener("click",function(){
-    console.log("submit button clicked");
+    // step 1: Get the email address inside the email input field
+    const emailField = document.getElementById("user-email");
+    const email = emailField.value;
+    // console.log(email);
+    
+    // step 2: Get the password inside the password input field
+    const passField = document.getElementById("user-password");
+    const pass = passField.value;
+    // console.log(pass);
+
+    // Danger: DO not verify email & password on the client side
+    // Step 3: Verify email & password
+    if(email === "rupok@sajmin.com" && pass === "secret"){
+        console.log("Valid User");
+    }
+    else{
+        console.log("Invalid User");
+    }
 })
